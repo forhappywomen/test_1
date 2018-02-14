6136519aac41d31c4a09faa6398b8bbdd63932c5
forhappywomen <forhappywomen@gmail.com>
2018-02-14 16:23:38 +0900

    `syncronize 되는지

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-console.log('hello...finally.plz');
 var steem = require("steem");
 steem.api.setOptions({url: 'https://api.steemit.com'});
-steem.api.getAccounts(['ned', 'dan'], function(err, response){
-    console.log(err, response);
-});
-console.log('hello...finally.??really??!!!Oh yes');
+
+var syn = require('synchronize');
+var fiber = sync.fiber;
+var await = syn.await;
+var defer = sync.defer;
+
+try {
+  fiber(function(){
+      var obj = await (steem.api.getAccounts(['ned','dan'], defer()));
+      console.log(obj);
+      obj = await(steem.api.getAccounts(['nhj12311'],defer()));
+      console.log(obj);
+    });
+  } catch(err){
+    console.log(err);
+  }
