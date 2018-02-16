bc512734e054743420250163c8da6b4ccf0480c9
forhappywomen <forhappywomen@gmail.com>
2018-02-16 23:05:11 +0900

    witson 추가

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+// winston log init!
+var winston = require('winston');
+require('winston-daily-rotate-file');
+const tsFormat = () => (new Date()).toLocaleTimeString();
+var logger = new (winston.Logger)({
+ transports: [
+   new (winston.transports.Console)({ timestamp: tsFormat }),
+   new (winston.transports.DailyRotateFile)({
+        // filename property 지정
+        name : 'log'
+        , filename: '/log/log.log'
+        , datePattern: '.yyyy-MM-dd'
+        , prepend: false
+        , timestamp: tsFormat
+        , level: 'info'
+        , json:false
+    }),
+   new (winston.transports.DailyRotateFile)({
+       name : 'error_log'
+       , filename: '/log/error.log'
+       , datePattern: '.yyyy-MM-dd'
+       , prepend: false
+       , timestamp: tsFormat
+       , level : 'error'
+       , json:false
+   })
+ ]
+});
+
+logger.log('info', 'log!!!');
+logger.info('info');
+logger.error('error logs');
+// winston log init end.
+
 var steem = require("steem");
 steem.api.setOptions({url: 'https://api.steemit.com'});
 
-var syn = require('synchronize');
+var sync = require('synchronize');
 var fiber = sync.fiber;
 var await = sync.await;
 var defer = sync.defer;
